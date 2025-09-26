d3bcc1c70193d66499e9532e234d3eb32b8cafd9
davidkan <davidkan717@gmail.com>
2025-09-25 21:03:26 -0400

    skeletons for other tables, created user, and billing info tables, deliverable 3 tables are ready to go

--- src/app/models/auditorium.py
+++ src/app/models/auditorium.py
@@ -1,12 +1,16 @@
 from .. import db
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.sql import func
-from .movie_category import movie_categories
 
 class Auditorium(db.Model):
     __tablename__ = "auditoriums"
+    __table_args__ = (
+        UniqueConstraint("name", name="uq_auditoriums_name"),
+    )
 
     auditorium_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False, index=True)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
-        return f"<Auditorium {self.title}>"
+        return f"<Auditorium {self.name}>"
