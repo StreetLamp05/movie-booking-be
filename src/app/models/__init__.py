451a5966d319e05ef4ed0d1be5cb8e971b70c074
davidkan <davidkan717@gmail.com>
2025-10-28 15:49:32 -0400

    cry

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -5,5 +5,10 @@ from .movie_category import movie_categories
 from .auditorium import Auditorium
 from .users import User
 from .showtimes import Showtime
-# from .billing_info import BillingInfo
 
+from .billing_info import BillingInfo
+from .reviews import Review
+from .seats import Seat
+from .seat_holds import SeatHold
+from .tickets import Ticket
+from .bookings import Booking
