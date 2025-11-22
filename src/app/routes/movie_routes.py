1c8856e20d1133bc9352adfc36aea05cb8085d85
QuentinProels <qap74567@uga.edu>
2025-11-22 17:33:26 -0500

    added delete functionality for manage movies page

--- src/app/routes/movie_routes.py
+++ src/app/routes/movie_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from ..controllers.movie_controller import create_movie, get_movie, get_movies
+from ..controllers.movie_controller import create_movie, get_movie, get_movies, delete_movie, update_movie
 
 bp = Blueprint("movie_routes", __name__, url_prefix="/movies")
 
@@ -10,6 +10,16 @@ def create():
 
 # GET /api/v1/movies/{movie_id}
 bp.get("/<int:movie_id>")(get_movie)
+
+# PUT /api/v1/movies/{movie_id}
+@bp.put("/<int:movie_id>")
+def update(movie_id):
+    return update_movie(movie_id)
+
+# DELETE /api/v1/movies/{movie_id}
+@bp.delete("/<int:movie_id>")
+def delete(movie_id):
+    return delete_movie(movie_id)
 
 # GET /api/v1/movies
 bp.get("")(get_movies)
